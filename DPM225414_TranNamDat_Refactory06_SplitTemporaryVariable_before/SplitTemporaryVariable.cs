580d64471b22f789e62fd8d559b36ed9dd5c2195
datdpm225414-rgb <dat_dpm225414@student.agu.edu.vn>
2025-10-19 22:00:57 +0700

    Add files via upload

---
+++ DPM225414_TranNamDat_Refactory06_SplitTemporaryVariable_before/SplitTemporaryVariable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225414_TranNamDat_Refactory06_SplitTemporaryVariable_before
+{
+    public class SplitTemporaryVariable
+    {
+        public static void Main()
+        {
+            int continiue = 0;
+            while (continiue == 0)
+            {
+                Console.WriteLine("Nhap chieu dai hinh chu nhat: ");
+                double height = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Nhap chieu rong hinh chu nhat: ");
+                double width = Convert.ToDouble(Console.ReadLine());
+                double temp = 2 * (height + width);
+                Console.WriteLine(temp);
+                temp = height * width;
+                Console.WriteLine(temp);
+                Console.WriteLine("Ban co muon tiep tuc tiep tuc, yes or no : ");
+                if (Console.ReadLine() == "no")
+                {
+                    continiue = 1;
+                }
+
+            }
+
+        }
+    }
+}
---
+++ DPM225414_TranNamDat_Refactory08_ReplaceMethodwithMethodObject_after/Order.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace DPM225414_TranNamDat_Refactory08_ReplaceMethodwithMethodObject_after
+{
+    public class Order
+    {
+        public int Quantity { get; set; }
+        public double ItemPrice { get; set; }
+
+        public Order(int quantity, double itemPrice)
+        {
+            Quantity = quantity;
+            ItemPrice = itemPrice;
+        }
+
+        public double Price()
+        {
+            return new PriceCalculator(this).Compute();
+        }
+    }
+
+    public class PriceCalculator
+    {
+        private  Order order;
+        private double primaryBasePrice;
+        private double secondaryBasePrice;
+        private double tertiaryBasePrice;
+
+        public PriceCalculator(Order Order)
+        {
+            order = Order;
+        }
+
+        public double Compute()
+        {
+            primaryBasePrice = order.Quantity * order.ItemPrice;
+            secondaryBasePrice = primaryBasePrice * 0.1;
+            tertiaryBasePrice = Math.Max(0, order.Quantity - 100) * order.ItemPrice * 0.05;
+
+            return primaryBasePrice - secondaryBasePrice + tertiaryBasePrice;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order(120, 50); 
+            Console.WriteLine("Giá cuối cùng: " + order.Price());
+        }
+    }
+}
