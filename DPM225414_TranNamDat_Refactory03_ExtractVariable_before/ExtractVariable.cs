cd06ee46eb312bbd0896f7c6b796e7b6b182d4e4
datdpm225414-rgb <dat_dpm225414@student.agu.edu.vn>
2025-10-19 21:56:34 +0700

    Add files via upload

---
+++ DPM225414_TranNamDat_Refactory03_ExtractVariable_before/ExtractVariable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225414_TranNamDat_Refactory03_ExtractVariable_before
+{
+    public class ExtractVariable
+    {
+
+
+        public bool wasInitialized()
+        {
+           
+            return true;
+        }
+        void RenderBanner(string platform,string browser, int resize)
+        {
+            if ((platform.ToUpper().IndexOf("MAC") > -1) &&
+                 (browser.ToUpper().IndexOf("IE") > -1) &&
+                  wasInitialized() && resize > 0)
+            {
+                Console.WriteLine("Rendering banner for MacOS and IE...");
+            }
+            else
+            {
+                Console.WriteLine("Conditions not met. Banner not rendered.");
+            }
+        }
+
+        public static void Main()
+        {
+            ExtractVariable extractVariable = new ExtractVariable();
+            Console.WriteLine("Starting banner rendering test...");
+            extractVariable.RenderBanner("MacOS", "IE11", 100);
+            extractVariable.RenderBanner("WinDows", "Chrome", 10);
+
+
+        }
+    }
+}
---
+++ DPM225414_TranNamDat_Refactory04_InlineTemp_before/InlineTemp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225414_TranNamDat_Refactory04_InlineTemp_before
+{
+    internal class InlineTemp
+    {
+
+
+        bool HasDiscount(Order order)
+        {
+            double basePrice = order.BasePrice();
+            return basePrice > 1000;
+        }
+        public static void Main()
+        {
+            Order order = new Order(5, 300);
+            InlineTemp inlineTemp = new InlineTemp();
+            bool hasDiscount = inlineTemp.HasDiscount(order);
+            Console.WriteLine("Has Discount: " + hasDiscount);
+        }
+    }
+}
