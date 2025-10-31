5a13d5a120fb70305fbcc7a2ebdfa227d279b587
datdpm225414-rgb <dat_dpm225414@student.agu.edu.vn>
2025-10-31 08:01:25 +0700

    Add files via upload

---
+++ DPM225414_TanNamDat_Refactory14_Hide Delegate_before/HideDelegate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225414_TanNamDat_Refactory14_Hide_Delegate_before
+{
+    internal class HideDelegate
+    {
+        public class Department
+        {
+            private string name;
+            private Person manager;
+
+            public Department(string name, Person manager)
+            {
+                this.name = name;
+                this.manager = manager;
+            }
+
+            public Person GetManager()
+            {
+                return manager;
+            }
+
+            public string GetName()
+            {
+                return name;
+            }
+        }
+
+        public class Person
+        {
+            private string name;
+            private Department department;
+
+            public Person(string name, Department department)
+            {
+                this.name = name;
+                this.department = department;
+            }
+
+            public Department GetDepartment()
+            {
+                return department;
+            }
+
+            public string GetName()
+            {
+                return name;
+            }
+        }
+
+        public class ClientClass
+        {
+            public static void Main()
+            {
+                Person manager = new Person("Nguyen Van A", null);
+                Department dept = new Department("IT Department", manager);
+                Person employee = new Person("Tran Nam Dat", dept);
+
+                Console.WriteLine("Employee: " + employee.GetName());
+                Console.WriteLine("Department: " + employee.GetDepartment().GetName());
+                Console.WriteLine("Manager: " + employee.GetDepartment().GetManager().GetName());
+            }
+        }
+    }
+}
---
+++ DPM225414_TanNamDat_Refactory14_HideDelegate_after/HideDelegate.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225414_TanNamDat_Refactory14_HideDelegate_after
+{
+    internal class HideDelegate
+    {
+        public class Department
+        {
+            private Person manager;
+
+            public Department(Person manager)
+            {
+                this.manager = manager;
+            }
+
+            public Person GetManager()
+            {
+                return manager;
+            }
+        }
+
+        public class Person
+        {
+            private string name;
+            private Department department;
+
+            public Person(string name, Department department)
+            {
+                this.name = name;
+                this.department = department;
+            }
+
+            public Person GetManager()
+            {
+                return department.GetManager();
+            }
+
+            public string GetName()
+            {
+                return name;
+            }
+        }
+        public static void Main()
+        {
+            Person manager = new Person("Nguyen Van A", null);
+            Department dept = new Department(manager);
+            Person employee = new Person("Tran Nam Dat", dept);
+
+            // Client gọi hàm getManager() của Person
+            Console.WriteLine("Manager: " + employee.GetManager().GetName());
+        }
+    }
+}
