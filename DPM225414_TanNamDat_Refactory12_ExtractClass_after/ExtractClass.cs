6ae1bb807885ab52dc4b2ac0cd3c76250bbd8441
datdpm225414-rgb <dat_dpm225414@student.agu.edu.vn>
2025-10-31 07:54:37 +0700

    Add files via upload

---
+++ DPM225414_TanNamDat_Refactory12_ExtractClass_after/ExtractClass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225414_TanNamDat_Refactory12_ExtractClass_after
+{
+    internal class ExtractClass
+    {    
+        public class TelephoneNumber
+        {
+            private string officeAreaCode;
+            private string Number;
+            
+            public TelephoneNumber(string officeAreaCode, string Number)
+            {
+                this.officeAreaCode = officeAreaCode;
+                this.Number = Number;
+            }
+            public string GetTelephoneNumber()
+            {
+                return Number;
+            }
+        }
+        public class Person
+        {
+            private string name;
+            private TelephoneNumber telephoneNumber;
+
+            public Person(string name, string officeAreaCode, string officeNumber)
+            {
+                this.name = name;
+               this.telephoneNumber = new TelephoneNumber(officeAreaCode, officeNumber);
+            }
+
+            public string GetName()
+            {
+                return name;
+            }
+
+            public string GetTelephoneNumber()
+            {
+                return telephoneNumber.GetTelephoneNumber() ;
+            }
+
+
+
+        }
+
+        public static void Main()
+        {
+            Person ps = new Person("Tran Nam Dat", "123", "0344589304");
+            string numPhone = ps.GetTelephoneNumber();
+            Console.WriteLine(numPhone);
+
+        }
+    }
+}
---
+++ DPM225414_TanNamDat_Refactory12_ExtractClass_before/ExtractClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225414_TanNamDat_Refactory12_ExtractClass_before
+{
+    internal class ExtractClass
+    {
+        public class Person
+        {
+            private string name;
+            private string officeAreaCode;
+            private string officeNumber;
+            public Person(string name, string officeAreaCode, string officeNumber)
+            {
+                this.name = name;
+                this.officeAreaCode = officeAreaCode;
+                this.officeNumber = officeNumber;
+            }
+            public string GetTelephoneNumber()
+            {
+
+                return officeNumber;
+            }
+        }
+        public static void Main()
+        {
+            Person ps = new Person("Tran Nam Dat", "123", "0344589304");
+            string numPhone = ps.GetTelephoneNumber();
+            Console.WriteLine(numPhone);
+
+        }
+    }
+}
+
