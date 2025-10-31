63ceec22334bd8c73cba5d6e82273f80862c6ac5
datdpm225414-rgb <dat_dpm225414@student.agu.edu.vn>
2025-10-31 07:46:55 +0700

    Add files via upload

---
+++ DPM225414_TanNamDat_Refactory09_SubstituteAlgorithm_after/SubstituteAlgorithm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225414_TanNamDat_Refactory09_SubstituteAlgorithm_after
+{
+    internal class SubstituteAlgorithm
+    {
+        string FoundPerson(string[] people)
+        {
+            List<string> candidates = new List<string>() { "Don", "John", "Kent" };
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                if (candidates.Contains(people[i]))
+                {
+                    return people[i];
+                }
+            }
+
+            return String.Empty;
+        }
+        public static void Main()
+        {
+            int n;
+            Console.WriteLine("Nhập vào độ dài mảng: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            string[] people = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Nhập vào đối tượng thứ " + (i + 1) + ": ");
+                people[i] = Console.ReadLine();
+            }
+
+            SubstituteAlgorithm sb = new SubstituteAlgorithm();
+            string result = sb.FoundPerson(people);
+
+            if (!string.IsNullOrEmpty(result))
+            {
+                Console.WriteLine("Kết quả: Đã tìm thấy " + result);
+            }
+            else
+            {
+                Console.WriteLine("Không tìm thấy Don, John hoặc Kent.");
+            }
+        }
+
+    }
+}
---
+++ DPM225414_TanNamDat_Refactory09_SubstituteAlgorithm_before/SubstituteAlgorithm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225414_TanNamDat_Refactory09_SubstituteAlgorithm_before
+{
+    internal class SubstituteAlgorithm
+    {
+        string FoundPerson(string[] people)
+        {
+            for (int i = 0; i < people.Length; i++)
+            {
+                if (people[i].Equals("Don"))
+                {
+                    return "Don";
+                }
+                if (people[i].Equals("John"))
+                {
+                    return "John";
+                }
+                if (people[i].Equals("Kent"))
+                {
+                    return "Kent";
+                }
+            }
+            return String.Empty;
+        }
+
+        public static void Main()
+        {
+            int n;
+            Console.WriteLine("Nhập vào độ dài mảng: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            string[] people = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Nhập vào đối tượng thứ " + (i + 1) + ": ");
+                people[i] = Console.ReadLine();
+            }
+            SubstituteAlgorithm sb = new SubstituteAlgorithm();
+            string result = sb.FoundPerson(people);
+            if (!string.IsNullOrEmpty(result))
+            {
+                Console.WriteLine("Kết quả: Đã tìm thấy " + result);
+            }
+            else
+            {
+                Console.WriteLine("Không tìm thấy Don, John hoặc Kent.");
+            }
+        }
+    }
+}
