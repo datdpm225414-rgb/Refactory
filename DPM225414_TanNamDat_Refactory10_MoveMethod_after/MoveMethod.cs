391b328af70429e45f72bed38483d12b16d6f3b5
datdpm225414-rgb <dat_dpm225414@student.agu.edu.vn>
2025-10-31 07:49:49 +0700

    Add files via upload

---
+++ DPM225414_TanNamDat_Refactory10_MoveMethod_after/MoveMethod.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225414_TanNamDat_Refactory10_MoveMethod_after
+{
+    internal class MoveMethod
+    {
+        class Class1
+        {
+        }
+
+        class Class2
+        {
+            public string SomeData = "Chao ban nhe";
+
+            public void aMethod()
+            {
+                Console.WriteLine("Phương thức aMethod đang sử dụng dữ liệu trong Class2: " + SomeData);
+            }
+        }
+
+        public static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8; 
+            Class2 cl2 = new Class2();
+            cl2.aMethod();
+        }
+    }
+}
---
+++ DPM225414_TanNamDat_Refactory10_MoveMethod_before/MoveMethod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225414_TanNamDat_Refactory10_MoveMethod_before
+{
+    internal class MoveMethod
+    {
+        class Class1
+        {
+            public void aMethod(Class2 c2)
+            {
+                Console.WriteLine(@"Phương thức aMethod đang sử dụng dữ liệu trong Class2: " + c2.SomeData);
+            }
+        }
+
+        class Class2
+        {
+            public string SomeData = "Chao ban nhe!";
+        }
+        public static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Class1 cl = new Class1();
+            Class2 cl2 = new Class2();
+            cl.aMethod(cl2);
+
+        }
+    }
+}
