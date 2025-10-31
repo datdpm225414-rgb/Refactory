1775b0f08f49598d31f1eeafcd0d431819531c67
datdpm225414-rgb <dat_dpm225414@student.agu.edu.vn>
2025-10-31 07:50:43 +0700

    Add files via upload

---
+++ DPM225414_TanNamDat_Refactory11_MoveField_after/MoveField.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225414_TanNamDat_Refactory11_MoveField_after
+{
+    internal class MoveField
+    {
+        class Class1
+        {
+        }
+
+        class Class2
+        {            
+            public string name = "Nguyễn Văn A";
+
+            public void ShowName()
+            {
+                Console.WriteLine("Tên là: " + name);
+            }
+        }
+        public static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Class2 cl2 = new Class2();
+            cl2.ShowName();
+
+        }
+    }
+}
---
+++ DPM225414_TanNamDat_Refactory11_MoveField_before/MoveField.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225414_TanNamDat_Refactory11_MoveField_before
+{
+    internal class MoveField
+    {
+        class Class1
+        {
+            public string name = "Nguyễn Văn A";
+        }
+
+        class Class2
+        {
+            public void ShowName(Class1 c1)
+            {
+                Console.WriteLine("Tên là: " + c1.name);
+            }
+        }
+        public static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Class1 cl = new Class1();
+            Class2 cl2 = new Class2();
+            cl2.ShowName(cl);
+
+        }
+
+    }
+}
