4248593e8c34bdaec419d8e4a500121e50a1934a
datdpm225414-rgb <dat_dpm225414@student.agu.edu.vn>
2025-11-02 19:22:44 +0700

    Add files via upload

---
+++ DPM225452_DoanTrongNhan_Refactory18_SelfEncapsulateField_After/DPM225452_DoanTrongNhan_Refactoty18_SelfEncapsulateField/Program.cs
@@ -0,0 +1,18 @@
+﻿class Range
+{
+    private int low, high;
+
+    int Low
+    {
+        get { return low; }
+    }
+    int High
+    {
+        get { return high; }
+    }
+
+    bool Includes(int arg)
+    {
+        return arg >= Low && arg <= High;
+    }
+}
