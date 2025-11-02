63fed9d1849498d13f7d7dab54b7a2cc90bfb87f
datdpm225414-rgb <dat_dpm225414@student.agu.edu.vn>
2025-11-02 19:55:52 +0700

    Add files via upload

---
+++ DPM225452_DoanTrongNhan_Refactory33_DecomposeConditional_Before/DPM225452_DoanTrongNhan_33_DecomposeConditional_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int date = 10;
+        int SUMMER_START = 6;
+        int SUMMER_END = 9;
+        int charge = 0;
+        int summerRate = 5;
+        int quantity= 4;
+        int winterRate = 4;
+        int winterServiceCharge = 5;
+
+        if (date < SUMMER_START || date > SUMMER_END)
+        {
+            charge = quantity * winterRate + winterServiceCharge;
+            
+        }
+        else
+        {
+            charge = quantity * summerRate;
+        }
+        Console.WriteLine("charge = "+charge);
+    }
+}
---
+++ DPM225452_DoanTrongNhan_Refactory34_HopNhatBieuThucDK_After/DPM225452_DoanTrongNhan_34_HopNhatBieuThucDK_2/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Program
+{
+    static double DisabilityAmount()
+    {
+        if(!IsNotEligibleForDisability())
+            return 0;
+        return 1;
+    }
+
+    static bool IsNotEligibleForDisability()
+    {
+        Random random = new Random();
+        int seniority = random.Next(-5, 5);
+        int monthsDisabled = random.Next(6, 20);
+        bool isPartTime = true;
+        if (seniority < 2)
+        {
+            return false;
+        }
+        if (monthsDisabled > 12)
+        {
+            return false;
+        }
+        if (isPartTime)
+        {
+            return false;
+        }
+        // Compute the disability amount.
+        // ...
+        return true;
+    }
+    public static void Main(string[] args)
+    {
+        if (DisabilityAmount() == 0)
+            Console.WriteLine("0 hehe");
+        else Console.WriteLine("ko phai 0");
+    }
+}
