75313865ac2fc215469900c5c9638c2dcc7d0941
datdpm225414-rgb <dat_dpm225414@student.agu.edu.vn>
2025-11-02 19:57:32 +0700

    Add files via upload

---
+++ DPM225452_DoanTrongNhan_Refactory34_HopNhatBieuThucDK_Before/DPM225452_DoanTrongNhan_34_HopNhatBieuThucDK_1/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Program
+{
+    static double DisabilityAmount()
+    {
+        Random random = new Random();
+        int seniority = random.Next(-5, 5);
+        int monthsDisabled = random.Next(6, 20);
+        bool isPartTime = true;
+        if (seniority < 2)
+        {
+            return 0;
+        }
+        if (monthsDisabled > 12)
+        {
+            return 0;
+        }
+        if (isPartTime)
+        {
+            return 0;
+        }
+        // Compute the disability amount.
+        // ...
+        return 1;
+    }
+    public static void Main(string[] args)
+    {
+        if (DisabilityAmount() == 0)
+            Console.WriteLine("0 hehe");
+        else Console.WriteLine("ko phai 0");
+    }
+}
---
+++ DPM225452_DoanTrongNhan_Refactory35_HopNhatDKTrung_After/DPM225452_DoanTrongNhan_35_HopNhatDKTrung_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+public class Program
+{
+    public static bool IsSpecialDeal()
+    {
+        Random random = new Random();
+        int t = random.Next(1, 10);
+        if (t < 6)
+            return true;
+        return false;
+    }
+    public static void Send()
+    {
+        Console.WriteLine("hello");
+    }
+    public static void Main(string[] args)
+    {
+        Random rand = new Random();
+        double price = rand.Next(1, 100);
+        double total = 0;
+        if (IsSpecialDeal())
+        {
+            total = price * 0.95;
+            //Send();
+        }
+        else
+        {
+            total = price * 0.98;
+            //Send();
+        }
+        Send();
+    }
+}
