acac6e130231977393dc7253a18953b4c347f912
datdpm225414-rgb <dat_dpm225414@student.agu.edu.vn>
2025-11-02 20:04:01 +0700

    Add files via upload

---
+++ DPM225452_DoanTrongNhan_Refactory37_DKLong2MenhDe_Before/DPM225452_DoanTrongNhan_37_DKLong2MenhDe_1/Program.cs
@@ -0,0 +1,55 @@
+﻿//Thay thế Điều kiện lồng nhau bằng Mệnh đề bảo vệ
+public class Test
+{
+    public bool isDead, isSeparated, isRetired = false;
+    public double DeadAmount() { return 10; }
+    public double SeparatedAmount() { return 8; }
+    public double RetiredAmount() { return 7; }
+    public double NormalPayAmount() { return 6; }
+    public double GetPayAmount()
+    {
+        double result;
+
+        if (isDead)
+        {
+            result = DeadAmount();//10
+        }
+        else
+        {
+            if (isSeparated)
+            {
+                result = SeparatedAmount();//8
+            }
+            else
+            {
+                if (isRetired)
+                {
+                    result = RetiredAmount();//7
+                }
+                else
+                {
+                    result = NormalPayAmount();//6
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Test test = new Test();
+        Random random = new Random();
+        int t = random.Next(0,2);
+        switch(t)
+        {
+            case 0: test.isDead = true; break;
+            case 1: test.isSeparated = true; break;
+            case 2: test.isRetired = true; break;
+        }
+        Console.WriteLine("PayAmount = " + test.GetPayAmount());
+    }
+}
---
+++ DPM225452_DoanTrongNhan_Refactory38_ThayDKThanhDaHinh_Before/DPM225452_DoanTrongNhan_38_ThayDKThanhDaHinh_1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Thay thế Điều kiện bằng Đa hình
+public class Bird
+{
+    public double GetSpeed()
+    {
+        int EUROPEAN = 1;
+        switch (type)
+        {
+            case EUROPEAN:
+                return GetBaseSpeed();
+            case AFRICAN:
+                return GetBaseSpeed() - GetLoadFactor() * numberOfCoconuts;//10,8,6
+            case NORWEGIAN_BLUE:
+                return isNailed ? 0 : GetBaseSpeed(voltage);
+            default:
+                throw new Exception("Should be unreachable");
+        }
+    }
+}
